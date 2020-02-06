ab9d94af42999526cc3840aafcb11c908038d68a
Robert <contact@kosro.de>
2020-02-06 02:20:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import threading
 
 app_thread = threading.Thread(target=app.run, daemon=True)
 
+
+def print_code(barcode):
+    print(barcode)
+
+
 if __name__ == '__main__':
     app_thread.start()
-    scan.run(print)
+    scan.run(print_code)
