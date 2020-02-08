e995d4c1ac66dde1f2daad3e83be33f7de5371e8
Robert <contact@kosro.de>
2020-02-08 14:00:53 +0100

    fixed it for real

--- scan.py
+++ scan.py
@@ -22,7 +22,8 @@ def run(callback):
         if ser.inWaiting() > 0:
             reading = ser.read(ser.inWaiting())
             line += reading
-            if reading == b'\n' and line != b'\n':
+            if b'\n' in line:
+                line = line.partition(b'\n')[0]
                 callback(line[:-3].decode())
                 line = b''
         sleep(0.01)
