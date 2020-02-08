b74f5c98a7667a75b6c8d34922812608b962d92a
Robert <contact@kosro.de>
2020-02-08 20:07:36 +0100

    didnt work, printing

--- app.py
+++ app.py
@@ -29,6 +29,7 @@ def send_code(barcode):
 @app.route('/cdp', methods=['POST'])
 def post_cdp():
     data = request.json
+    print(jsonify(data))
     top = ''
     bottom = ''
     if 'top' in data:
